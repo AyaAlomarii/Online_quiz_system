fcb351f735b4ed24a676377ea1bb1208ccdb3b23
AyaAlomarii <ayah.aliomari@gmail.com>
2024-10-06 23:17:14 +0300

    edit layout

--- src/layouts/StudentLayout.vue
+++ src/layouts/StudentLayout.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-layout  class="bg-bg-grey">
+  <q-layout class="bg-bg-grey">
     <!-- Header -->
     <q-card class="height-365 q-ma-md br-8">
       <q-card-section class="no-padding">
@@ -8,21 +8,18 @@
 
       <q-card-section class="row">
         <q-avatar class="col-auto q-pa-xs margin-t--50" size="128px" square>
-          <q-img
-            src="/student/avatar.svg"
-            class="width-120 height-120"
-          />
+          <q-img src="/student/avatar.svg" class="width-120 height-120" />
         </q-avatar>
-        <div class="col column  ">
-          <div class="text-h5">aya</div>
+        <div class="col column">
+          <div class="text-h6">{{ userInfo?.username || 'Unknown User' }}</div>
 
           <div class="row justify-start items-center q-py-sm grey-text">
             <div class="text-body-1 text-weight-500">
-              <q-icon name="person" size="sm" class="q-mr-xs"/> aya
+              <q-icon name="person" size="sm" class="q-mr-xs" />{{ userInfo?.username || 'Unknown User' }}
             </div>
             <div class="text-body-1 text-weight-500 q-px-md">
-              <q-icon name="email" size="sm" class="q-mr-xs"/>
-              aya@gmail.com
+              <q-icon name="email" size="sm" class="q-mr-xs" />
+              {{ userInfo?.email || 'No email available' }}
             </div>
 
             <q-space />
@@ -32,20 +29,34 @@
       </q-card-section>
     </q-card>
 
-
-
-
     <q-page-container>
       <router-view />
-
     </q-page-container>
     <!-- footer -->
 
-    <q-footer borderless class=" row justify-center bg-bg-grey heigh-54px fixed-bottom ">
-     <div class="absolute-center text-secondary text-body2 text-center">
-      © 2024, Made with by <span class="text-primary">Digital Pages`-</span>
-     </div>
-      </q-footer>
+    <q-footer
+      borderless
+      class="row justify-center bg-bg-grey heigh-54px fixed-bottom"
+    >
+      <div class="absolute-center text-secondary text-body2 text-center">
+        © 2024, Made with by <span class="text-primary">Digital Pages</span>
+      </div>
+    </q-footer>
   </q-layout>
 </template>
 
+<script setup lang="ts">
+import UserModel from 'src/models/UserModel';
+import { LocalStorage } from 'quasar';
+import { onMounted, ref } from 'vue';
+
+// variables
+const userInfo = ref<UserModel | null>(null); // Initialize with null
+
+onMounted(() => {
+  const storedUser = LocalStorage.getItem('currentUser') as UserModel | null;
+  if (storedUser) {
+    userInfo.value = storedUser;
+  }
+});
+</script>
